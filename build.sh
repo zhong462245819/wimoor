e3cd3b4f1976247c7a0448f41422996fdc49d6ed
钟源 <462245819@qq.com>
2025-12-08 18:07:38 +0800

    init install

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 CONF="services.conf"
 
-while read -r servicePath; do
+while IFS= read -r servicePath || [[ -n "$servicePath" ]]; do
   [[ -z "$servicePath" || "$servicePath" =~ ^# ]] && continue
 
   serviceName=$(basename "$servicePath")
@@ -15,7 +15,7 @@ while read -r servicePath; do
 
   echo "当前目录：$(pwd)"
 
-  docker build -t "$serviceName:latest" .
+#  docker build -t "$serviceName:latest" .
 
   cd - > /dev/null
 
